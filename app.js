0b4d6aea20e1217cec745191a19bf28f6de0e3c5
Humphries <whumphries@una.edu>
2023-08-24 12:56:28 -0500

    cleaned up app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World from express')
+  res.send('Hello World from express and a PaaS/Render ')
 })
 
 app.listen(3000)
