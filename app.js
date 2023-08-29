42da60657f85e9c4f2d5b864c54c2659aedb118b
Humphries <whumphries@una.edu>
2023-08-29 13:45:39 -0500

    get salute

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
+app.set('view engine' , 'ejs');
+
+let myVariableServer = "soft code  Server data";
+app.get('/view', function (req, res) {
+  res.render('index', {'myVariableClient' : myVariableServer});
+})
 
 app.get('/', function (req, res) {
   res.send('Hello World from express and a PaaS/Render ')
 })
+
+app.get('/index', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
+
 
 app.listen(3000)
